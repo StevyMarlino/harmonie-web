139d80bc831c74dd94883f74e0b86ec4e4fbe681
Stephen Jacques <jacquesstephan10@gmail.com>
2024-06-19 16:35:18 +0100

    Intégration du site vitrine

---
+++ resources/views/layouts/lastest-posts.blade.php
@@ -0,0 +1,37 @@
+<section class="section section-sm bg-default" id="news">
+  <div class="container">
+    <h2>Latest blog posts</h2>
+    <div class="row row-45">
+      <div class="col-sm-6 col-lg-4 wow fadeInLeft">
+        <!-- Post Modern-->
+        <article class="post post-modern"><a class="post-modern-figure" href="#"><img src="{{asset('vitrine/images/post-10-370x307.jpg')}}" alt="" width="370" height="307"/>
+            <div class="post-modern-time">
+              <time datetime="2019-07-04"><span class="post-modern-time-month">07</span><span class="post-modern-time-number">04</span></time>
+            </div></a>
+          <h4 class="post-modern-title"><a href="#">Benefits of Async/Await</a></h4>
+          <p class="post-modern-text">Asynchronous functions are a good and bad thing in JavaScript. The good side is that asynchronous functions are non-blocking and...</p>
+        </article>
+      </div>
+      <div class="col-sm-6 col-lg-4 wow fadeInLeft" data-wow-delay=".1s">
+        <!-- Post Modern-->
+        <article class="post post-modern"><a class="post-modern-figure" href="#"><img src="{{ asset('vitrine/images/post-11-370x307.jpg')}}" alt="" width="370" height="307"/>
+            <div class="post-modern-time">
+              <time datetime="2019-07-17"><span class="post-modern-time-month">07</span><span class="post-modern-time-number">17</span></time>
+            </div></a>
+          <h4 class="post-modern-title"><a href="#">Key Considerations of iPaaS</a></h4>
+          <p class="post-modern-text">Digital transformation requires cloud appropriate adoption, legacy IT systems modernization, and Agile-based methodologies for faster...</p>
+        </article>
+      </div>
+      <div class="col-sm-6 col-lg-4 wow fadeInLeft" data-wow-delay=".2s">
+        <!-- Post Modern-->
+        <article class="post post-modern"><a class="post-modern-figure" href="#"><img src="{{ asset('vitrine/images/post-12-370x307.jpg')}}" alt="" width="370" height="307"/>
+            <div class="post-modern-time">
+              <time datetime="2019-07-22"><span class="post-modern-time-month">07</span><span class="post-modern-time-number">22</span></time>
+            </div></a>
+          <h4 class="post-modern-title"><a href="#">Never Stop Optimizing Your Code</a></h4>
+          <p class="post-modern-text">In this tutorial, we will discuss the Hibernate Query Language. HQL is an object-oriented query language. Hibernate Query...</p>
+        </article>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ resources/views/layouts/page.blade.php
@@ -0,0 +1,14 @@
+<div id="home">
+    @include('layouts.home.header')
+    @include('layouts.home.swiper')
+</div>
+
+@include('layouts.services')
+
+@include('layouts.cta')
+
+@include('layouts.experience')
+
+@include('layouts.lastest-posts')
+
+@include('layouts.footer')
