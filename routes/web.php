6368b7797f60a8ea4c2e027182f40f0c8b19ea48
Stevy Endaman <stevyabessolo@gmail.com>
2024-06-22 17:41:53 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,14 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use TCG\Voyager\Facades\Voyager;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+/**
+ * Route site gabon
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [SiteController::class, 'home'])->name('home');
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
