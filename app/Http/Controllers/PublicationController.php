c2d433212408aa07284b13459d204f88ce13cacb
Stevy Endaman <stevyabessolo@gmail.com>
2024-06-22 22:13:19 +0200

    Update PublicationController.php

--- app/Http/Controllers/PublicationController.php
+++ app/Http/Controllers/PublicationController.php
@@ -4,14 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use TCG\Voyager\Models\Post;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class PublicationController extends Controller
 {
     public function index()
     {
+        $postsCollection = Post::orderBy('created_at', 'desc')->where('status', 'PUBLISHED')->get();
+        
+        // Paginer la collection
+        $currentPage = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = 4; // Nombre de posts par page
+        $currentPagePosts = $postsCollection->slice(($currentPage - 1) * $perPage, $perPage)->all();
+        
+        $posts = new LengthAwarePaginator($currentPagePosts, $postsCollection->count(), $perPage);
+        $posts->setPath(route('publications'));
+
         $data = [
             'title' => 'Publications',
-            'posts' => Post::orderBy('created_at', 'desc')->get(),
+            'posts' => $posts,
             'publications' => Post::orderBy('created_at', 'desc')->take(3)->where('status', 'PUBLISHED')->get()
         ];
 
