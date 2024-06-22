29cfb3f9a2b565315a6330cc0a29fdc4d742b51e
Stevy Endaman <stevyabessolo@gmail.com>
2024-06-22 20:44:23 +0200

    Update SiteController.php

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Slide;
 use Illuminate\Http\Request;
+use TCG\Voyager\Models\Post;
 
 class SiteController extends Controller
 {
     public function home()
     {
         $data = [
-          'slides' => Slide::latest()->take(3)->get(),
+            'title' => 'Accueil',
+            'slides' => Slide::latest()->take(3)->get(),
+            'posts' => Post::orderBy('created_at', 'desc')->take(3)->where('status', 'PUBLISHED')->get()
         ];
-        return view('index', $data );
+        return view('index', $data);
     }
 }
