a382a9d5c65f2450402187f29be7a956d0c6414a
Stevy Endaman <stevyabessolo@gmail.com>
2024-06-22 23:51:34 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\MissionController;
 use App\Http\Controllers\PublicationController;
 use App\Http\Controllers\SiteController;
+use App\Http\Controllers\WordPresidentController;
 use TCG\Voyager\Facades\Voyager;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [SiteController::class, 'home'])->name('home');
 Route::get('/publications',[PublicationController::class,'index'])->name('publications');
 Route::get('/publication-details/{slug}', [PublicationController::class,'details'])->name("publication-details");
+
+Route::get('/mission',[MissionController::class,'index'])->name('mission');
+Route::get('/mot-du-premier-president',[WordPresidentController::class,'index'])->name('mot.pp');
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
