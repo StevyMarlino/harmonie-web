13cc9281d19e763ce7a352b01da3b8ee6f1a4978
Stevy Endaman <stevyabessolo@gmail.com>
2024-06-22 20:46:44 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PublicationController;
 use App\Http\Controllers\SiteController;
 use TCG\Voyager\Facades\Voyager;
 use Illuminate\Support\Facades\Route;
@@ -9,6 +10,8 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', [SiteController::class, 'home'])->name('home');
+Route::get('/publications',[PublicationController::class,'index'])->name('publications');
+Route::get('/publication-details/{slug}', [PublicationController::class,'details'])->name("publication-details");
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
