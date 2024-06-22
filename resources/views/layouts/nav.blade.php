f10c143f626731026dc25f4ca79d89c116b0e55b
Stevy Endaman <stevyabessolo@gmail.com>
2024-06-22 23:51:24 +0200

    Update nav.blade.php

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -11,19 +11,19 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle {{ request()->routeIs('home') ? 'active' : '' }}" href="#" id="navbarDropdown" role="button"
+                    <a class="nav-link dropdown-toggle {{ request()->routeIs('mission') | request()->routeIs('mot.pp') ? 'active' : '' }}" href="#" id="navbarDropdown" role="button"
                         data-bs-toggle="dropdown" aria-expanded="false">
                         Qui sommes-nous?
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Mot du PP</a></li>
-                        <li><a class="dropdown-item" href="#">Missions</a></li>
+                        <li><a class="dropdown-item" href="{{ route('mot.pp') }}">Mot du PP</a></li>
+                        <li><a class="dropdown-item" href="{{ route('mission') }}">Missions</a></li>
                     </ul>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="about.html">Nos Décisions</a></li>
-                <li class="nav-item"><a class="nav-link" href="work.html">Vos Demandes/Démarches</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Nos Décisions</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Vos Demandes/Démarches</a></li>
                 <li class="nav-item"><a class="nav-link {{ request()->routeIs('publications') || request()->routeIs('publication-details') ? 'active' : '' }}" href="{{ route('publications') }}">Publications</a></li>
-                <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
             </ul>
         </div>
     </div>
