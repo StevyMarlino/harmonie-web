56a17219f7ebae297e4ccf69158820c24c2bb16d
Stevy Endaman <stevyabessolo@gmail.com>
2024-06-22 20:46:05 +0200

    Create footer.blade.php

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,67 @@
+<footer class="ftco-footer img" style="background-image: url(images/bg_1.jpg);">
+    <div class="overlay"></div>
+    <div class="container-xl">
+        <div class="row mb-5 justify-content-between">
+            <div class="col-md-6 col-lg">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2 logo d-flex">
+                        <a class="navbar-brand" href="index-2.html">
+                            <img src="{{ asset('images/conseil-etat-logo.png') }}" alt="conseil etat logo">
+                        </a>
+                    </h2>
+                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.
+                    </p>
+                    <ul class="ftco-footer-social mt-4">
+                        <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+                        <li><a href="#"><span class="fa fa-facebook"></span></a></li>
+                        <li><a href="#"><span class="fa fa-instagram"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">Services</h2>
+                    <div class="d-md-flex">
+                        <ul class="list-unstyled w-100">
+                            <li><a href="#"><span
+                                        class="ion ion-ios-arrow-round-forward me-2"></span>Mot du pp</a>
+                            </li>
+                            <li><a href="#"><span
+                                        class="ion ion-ios-arrow-round-forward me-2"></span>Nos missions</a></li>
+                            <li><a href="#"><span
+                                        class="ion ion-ios-arrow-round-forward me-2"></span>Organisation</a></li>
+                            <li><a href="#"><span class="ion ion-ios-arrow-round-forward me-2"></span>Publications</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-6 col-lg">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">Une Question?</h2>
+                    <div class="block-23 mb-3">
+                        <ul>
+                            <li><span class="icon fa fa-map marker"></span><span class="text">Localisation : Gabon, Libreville</span></li>
+                            <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid px-0 py-5 bg-darken">
+        <div class="container-xl">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <p class="mb-0" style="font-size: 14px;">Copyright &copy;
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script> All rights reserved | <i
+                            class="fa fa-heart color-danger" aria-hidden="true"></i> by <a
+                            href="#" target="_blank" rel="nofollow noopener">SM</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
