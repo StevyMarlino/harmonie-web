35653b69df5da84f0aa5dca2390d768aba2e11af
Stevy Endaman <stevyabessolo@gmail.com>
2024-06-25 00:29:30 +0200

    Create SlideFactory.php

---
+++ database/factories/SlideFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Slide>
+ */
+class SlideFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->title(),
+            'excerpt' => fake()->text(15),
+            'image' => fake()->imageUrl()
+        ];
+    }
+}
