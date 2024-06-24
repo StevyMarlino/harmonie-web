f52951127e6ce0ad504d8de7f78cad7f60bf6719
Stevy Endaman <stevyabessolo@gmail.com>
2024-06-25 00:11:55 +0200

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,18 +3,17 @@
 namespace Tests\Feature;
 
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    use RefreshDatabase;
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/publications');
 
         $response->assertStatus(200);
     }
