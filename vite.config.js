4e6483008a43e940719b6c76adf824568befa324
Stevy Endaman <stevyabessolo@gmail.com>
2024-06-25 17:57:15 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+const folder = {
+    src: "resources/", // source files
+    src_assets: "resources/", // source assets files
+    dist: "public/", // build files
+    dist_assets: "public/build/" //build assets files
+};
+
 export default defineConfig({
+    build: {
+        manifest: true,
+        rtl: true,
+        outDir: folder.dist_assets,
+        cssCodeSplit: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css'],
