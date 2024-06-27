3dd7f45a64bc9e21d7f0d053b1e78e6280e7b5a6
Stevy Endaman <stevyabessolo@gmail.com>
2024-06-27 22:25:58 +0200

    Create TrackVisitors.php

---
+++ app/Http/Middleware/TrackVisitors.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\Visitor;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrackVisitors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        Visitor::create([
+            'ip' => $request->ip(),
+            'url' => $request->url(),
+            'visited_at' => now(),
+        ]);
+        return $next($request);
+    }
+}
