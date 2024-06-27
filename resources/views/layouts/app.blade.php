e324426b4c3436e4ba8389ab91117eb1a7fdce90
Stevy Endaman <stevyabessolo@gmail.com>
2024-06-27 21:48:32 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <title> {{ setting('site.title') . ' | ' . $title }} </title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="{{ URL::asset('build/assets/app-B1oTZMXc.css') }}">   
+    <link rel="stylesheet" href="{{ URL::asset('build/assets/app-BRNvwAmS.css') }}">   
     {{-- @vite(['resources/css/app.css']) --}}
 
 </head>
